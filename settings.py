7e83d5634582919a859c2df880579a318ed8da0f
Clayton <cdston@gmail.com>
2022-07-11 18:27:21 -0300

    cridado arquivo de configuração da tela

---
+++ settings.py
@@ -0,0 +1,9 @@
+"""Uma classe para armazenar todas as configurações da Invasão Alienígena"""
+class Settings():   
+    
+    # Configuraçoes da tela
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
