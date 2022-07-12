96e01be798bf86565270aeb582ae3c04dab5c496
Clayton <cdston@gmail.com>
2022-07-12 10:44:52 -0300

    colocando limite na borda

--- ship.py
+++ ship.py
@@ -21,13 +21,12 @@ class Ship():
     
     # Atualiza posição da espaconave de acordo com a flag de movimento
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-            #self.rect.centerx += 1
-        if self.moving_left:
+                
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-            #self.rect.centerx -= 1
-
+           
         self.rect.centerx = self.center
 
 
